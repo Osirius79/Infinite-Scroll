9e325ba6b485222317eaaa909552ae2e12e09e14
Pavel Vorontsov <pvorontsovp@gmail.com>
2021-05-06 20:10:19 -0400

    add unsplash api in jsscript

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 // Usplash API
-const apiUrl = `https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY`
+const count = 10;
+const apiKey = 'DZoa-hhg8ZUaCk6k90i_XZH9Fdiqwobr5KWJzcn1Bqc';
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        //Catch here
+    }
+}
+
+//On Load
+getPhotos();
